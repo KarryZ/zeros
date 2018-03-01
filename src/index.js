4ed241f2acb9cfea3e97c83d147b5c552bd7f5b1
Kate Zasulevich <katejidkova@gmail.com>
2018-03-01 23:55:32 +0300

    now we catch all nulls

--- src/index.js
+++ src/index.js
@@ -1,36 +1,11 @@
-//module.exports = 
-	function getZerosCount(number) {
+module.exports = function getZerosCount(number) {
 	// your implementation
-	let l = 1;
-	let r = number;
-  let res; 
-	function ProdTree(l, r) {
-		if (l > r) return 1;
-		if (l == r) return l;
-		if (r - l == 1) return l * r;
-		let m = Math.round( (l + r) / 2 );
-		
-		return  res = ProdTree(l, m) * ProdTree(m + 1, r) ;
-		
-		 
+	let res = 0;
+	while (number > 2) {
+		number /= 5;
+		res += Math.floor(number);
 	}
-		
-//console.log(res);
-	function FactTree(n) {
-		if (n < 0) return 0;
-		if (n == 0) return 1;
-		if (n == 1 || n == 2) return n;
-		return ProdTree(2, n);
-	};
-	FactTree(r);
-		
-  
-
- 
-	
-	let factorialNum = res + "";
-	//document.write(factorialNum.match(/0+$/ig)[0].length);
-	console.log(+factorialNum.match(/0+$/ig)[0].length);
+	//console.log(res);
+	return res;
 };
-
-getZerosCount()
+//getZerosCount(15949280)//3987315
